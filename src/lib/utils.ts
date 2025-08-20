9d6743f4770c69528c652a1964dd6b0a5a21e97b
Spark <vraymond@sparkfusiondigital.com>
2025-08-20 09:36:00 -0400

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,6 @@
-export const cn=(...xs:Array<string|false|null|undefined>)=>xs.filter(Boolean).join(' ');
-export const clamp=(n:number,min=0,max=Number.POSITIVE_INFINITY)=>Math.min(max,Math.max(min,n));
-export const noop=()=>{};
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
